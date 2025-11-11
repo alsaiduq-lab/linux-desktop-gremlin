472195862194e76ac7619417faec3caa66a0d85b
Xgameisdabest <lemonadepham@gmail.com>
2025-11-11 11:03:11 +0700

    updated gremlin_picker.sh, fixes the script so that it can be launched from anywhere (such as a desktop file)

--- gremlin_picker.sh
+++ gremlin_picker.sh
@@ -2,8 +2,11 @@
 
 # an extremely simple gremlin picker using rofi
 
-# list all gremlins in spritesheet
-available_gremlins=$(command ls -1 spritesheet)
+# get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# list all gremlins in spritesheet (relative to script dir)
+available_gremlins=$(command ls -1 "$SCRIPT_DIR/spritesheet" 2>/dev/null)
 
 # use rofi to pick the selected gremlin
 pick=$(echo -e "$available_gremlins" | rofi -dmenu)
@@ -17,16 +20,16 @@ if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
 	#check whether if they want to use uv or not
 	pick_uv=$(echo -e "Uv\nNon-uv" | rofi -dmenu)
 	if [[ $pick_uv == "Uv" ]]; then
-		./run-uv-xwayland.sh $pick
+		"$SCRIPT_DIR/run-uv-xwayland.sh" "$pick"
 	elif [[ $pick_uv == "Non-Uv" ]]; then
-		./run-xwayland.sh $pick
+		"$SCRIPT_DIR/run-xwayland.sh" "$pick"
 	fi
 elif [[ $XDG_SESSION_TYPE == "x11" ]]; then
 	#check whether if they want to use uv or not
 	pick_uv=$(echo -e "Uv\nNon-uv" | rofi -dmenu)
 	if [[ $pick_uv == "Uv" ]]; then
-		./run-uv-x11.sh $pick
+		"$SCRIPT_DIR/run-uv-x11.sh" "$pick"
 	elif [[ $pick_uv == "Non-Uv" ]]; then
-		./run-x11.sh $pick
+		"$SCRIPT_DIR/run-x11.sh" "$pick"
 	fi
 fi
